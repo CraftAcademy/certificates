f9fa4bd3eb49f1b8c065914d5f79391c5067daa0
Raoul DIFFOUO <diraulo@gmail.com>
2018-04-14 08:51:58 +0200

    Add support for two types of certificates

--- lib/csv_parse.rb
+++ lib/csv_parse.rb
@@ -3,21 +3,31 @@ require 'csv'
 module CSVParse
   def self.import(file, obj, parent)
     csv_import_opts = {
-      quote_char: '"', col_sep: ';', row_sep: :auto, headers: true,
-      header_converters: :symbol, converters: :all, encoding: 'UTF-8'
+      col_sep: ';',
+      converters: :all,
+      encoding: 'UTF-8',
+      headers: true,
+      header_converters: :symbol,
+      quote_char: '"',
+      row_sep: :auto
     }
 
     import = CSV.read(file, csv_import_opts).collect do |row|
       Hash[row.collect { |c, r| [c, r] }]
     end
+
     create_instance(parent, obj, import)
   end
 
   def self.create_instance(parent, obj, dataset)
     dataset.each do |data|
-      student = obj.first_or_create({ full_name: data[:full_name] },
-                                    full_name: data[:full_name],
-                                    email: data[:email])
+      student = obj.first_or_create(
+        { full_name: data[:full_name] },
+        full_name: data[:full_name],
+        email: data[:email],
+        completed: data[:completed]
+      )
+
       student.deliveries << parent unless student.deliveries.include? parent
       student.save
     end
--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,7 @@ class Student
   property :id, Serial
   property :full_name, String
   property :email, String, required: true
+  property :completed, Boolean, default: true
 
   has n, :deliveries, through: Resource
   has n, :certificates
